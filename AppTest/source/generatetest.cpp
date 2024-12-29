83a0a9951f911d9cc7725d299083e7b3ec2ac25b
Adam Spivack <spivack@outlook.com>
2024-12-29 13:36:12 -0800

    Simplified generate test

--- AppTest/source/generatetest.cpp
+++ AppTest/source/generatetest.cpp
@@ -1,27 +1,16 @@
 
 #include "generatetest.hpp"
-#include <algorithm>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <random>
-#include <ranges>
-#include <sstream>
-#include <string_view>
-#include <variant>
 #include "TestConfig.hpp"
-
-using std::operator""sv;
+#include <iostream>
 
 int generate_tests_cmd_line(std::filesystem::path test_output, CommandLine::TestModes mode, bool huge, bool overwrite)
 {
     auto currentPath = std::filesystem::current_path();
     std::cout << "Current Path: " << currentPath << '\n';
-    
-    bool noErrors = mode == CommandLine::TestModes::NoErrors;
-    Tests::Configuration config { Tests::Configuration::generate_default( noErrors, huge )};
 
-    
+    bool noErrors = mode == CommandLine::TestModes::NoErrors;
+    Tests::Configuration config{Tests::Configuration::generate_default(noErrors, huge)};
+
     config.write_all_tests(std::filesystem::current_path(), false);
 
     return 0;
