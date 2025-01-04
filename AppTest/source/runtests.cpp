28ad401ee5b16bfdc88a9c46cf754f1912bbbe36
Fearless40 <spivack@gmail.com>
2025-01-03 23:07:38 -0800

    Added code framework code to run tests.

--- AppTest/source/runtests.cpp
+++ AppTest/source/runtests.cpp
@@ -1,7 +1,29 @@
 #include "runtests.hpp"
 #include "TestConfigTOML.hpp"
+#include "TestDefinition.hpp"
 #include <iostream>
+#include <chrono>
 
+
+struct TestResult {
+    std::chrono::duration timeToRun;
+    Tests::Definition & def; 
+    std::string programOutput; 
+    bool passed; 
+};
+
+TestResult run_test( const Tests::Configuration::ExpectedResults & expected, std::filesystem::path app ) {
+
+} 
+
+std::vector<TestResult> run_all_tests( Tests::Configuration & config, std::filesystem::path app ){
+    
+    std::vector<TestResult> results;
+    for( auto const & expected : config ){
+        results.push_back(run_test(expected, app));
+    }
+
+}  
 
 int main_run_tests( std::filesystem::path testPath, std::filesystem::path exe ){
 
@@ -9,16 +31,10 @@ int main_run_tests( std::filesystem::path testPath, std::filesystem::path exe ){
 
     loadMe = Tests::toml_file_to_config( testPath );
 
-    std::filesystem::path newFile = testPath;
-
-    newFile.replace_filename("rewritten.txt"); 
-
+    
     std::cout << "Finished Loading: " << testPath << '\n';
-    std::cout << "Regenerating the config file from loaded file: " << newFile << '\n';
+    
 
-    Tests::config_to_toml_file( loadMe, newFile);
-
-    std::cout << "Finished\n";
 
     return 0;
 }
