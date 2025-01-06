065038925a30934593cca30b544af06e649e438f
Adam Spivack <spivack@outlook.com>
2025-01-05 22:20:41 -0800

    Works better with just _popen could not get the subprocess library to work.

--- AppTest/source/runtests.cpp
+++ AppTest/source/runtests.cpp
@@ -2,8 +2,10 @@
 #include "TestConfigTOML.hpp"
 #include "TestDefinition.hpp"
 #include "subprocess.hpp"
+#include <algorithm>
 #include <chrono>
 #include <iostream>
+#include <stdio.h>
 
 struct TestResult
 {
@@ -26,16 +28,59 @@ void run_test(const Tests::Configuration::ExpectedResults &expected, std::string
         guesses.append(" ");
     }
 
-    std::vector<std::string> arguments{app, "--load", expected.filename};
+    std::vector<std::string> arguments{app, "--load", expected.filename, "--guess"};
     if (expected.queries.size() > 0)
     {
-        arguments.emplace_back("--guess");
+
         arguments.push_back(guesses);
     }
-    std::cout << "Running the following test: " << app << " --load " << expected.filename << " --guess " << guesses
-              << '\n';
-    auto result = sp::check_output(arguments);
-    std::cout << result.buf.data() << '\n';
+    else
+    {
+        arguments.push_back("a0");
+    }
+    std::string arg2;
+    for (const auto &st : arguments)
+    {
+        arg2 += st + " ";
+    };
+
+    std::cout << "Running the following test: " << arg2 << '\n';
+    // auto result = sp::check_output(arguments);
+    // auto result = sp::Popen(arguments, sp::output{sp::PIPE});
+    // auto buf = result.communicate().first;
+    // std::cout << buf.buf.data() << '\n';
+
+    char psBuffer[128];
+    FILE *pPipe;
+
+    /* Run DIR so that it writes its output to a pipe. Open this
+     * pipe with read text attribute so that we can read it
+     * like a text file.
+     */
+
+    if ((pPipe = _popen(arg2.c_str(), "rt")) == NULL)
+    {
+        exit(1);
+    }
+
+    /* Read pipe until end of file, or an error occurs. */
+
+    while (fgets(psBuffer, 128, pPipe))
+    {
+        std::cout << psBuffer;
+    }
+
+    int endOfFileVal = feof(pPipe);
+    int closeReturnVal = _pclose(pPipe);
+
+    if (endOfFileVal)
+    {
+        printf("\nProcess returned %d\n", closeReturnVal);
+    }
+    else
+    {
+        printf("Error: Failed to read the pipe to the end.\n");
+    }
 }
 
 std::vector<TestResult> run_all_tests(Tests::Configuration &config, std::filesystem::path app)
