6103e3e8991baf2def56dbf4680161a82d081e6d
Adam Spivack <spivack@outlook.com>
2025-01-06 22:31:11 -0800

    trying a different process library. It seems faster but still working out how to use it.

--- AppTest/source/runtests.cpp
+++ AppTest/source/runtests.cpp
@@ -1,12 +1,16 @@
 #include "runtests.hpp"
 #include "TestConfigTOML.hpp"
 #include "TestDefinition.hpp"
-#include "subprocess.hpp"
+// #include "subprocess.hpp"
 #include <algorithm>
 #include <atomic>
 #include <chrono>
 #include <iostream>
+#include <reproc++/drain.hpp>
+#include <reproc++/reproc.hpp>
 #include <thread>
+
+using namespace std::chrono_literals;
 
 struct TestResult
 {
@@ -24,13 +28,46 @@ struct ExecuteResult
     bool timeOut{false};
 };
 
-ExecuteResult execute_app(const std::string &arguments)
+ExecuteResult execute_app(const std::vector<std::string> &arguments)
+{
+    reproc::stop_actions stopActions{{reproc::stop::kill, 500ms}, {reproc::stop::terminate, 1000ms}, {}};
+
+    std::string output;
+    reproc::options options;
+    options.stop = stopActions;
+    options.deadline = 500ms;
+    reproc::process process;
+
+    std::error_code ec = process.start(arguments, options);
+
+    if (ec == std::errc::no_such_file_or_directory)
+    {
+        std::cerr << "Program not found. Make sure it's available from the PATH. \n";
+        return {"", -1, true, false};
+    }
+
+    reproc::sink::string sink(output);
+
+    ec = reproc::drain(process, sink, reproc::sink::null);
+    if (ec)
+    {
+        std::cout << ec.value() << '\n';
+    }
+
+    int status = 0;
+    std::tie(status, ec) = process.wait(reproc::infinite);
+
+    std::cout << output << '\n';
+    return {output, 0, false, false};
+}
+
+/*ExecuteResult execute_app(const std::string &arguments)
 {
     namespace sp = subprocess;
     auto result = sp::Popen(arguments, sp::output{sp::PIPE}, sp::bufsize{2048});
     std::string appResult;
     std::atomic<bool> programFinished{false};
-    /* Read pipe until end of file, or an error occurs. */
+
     // If it takes longer than 1 min then quit with a time out.
     auto start = std::chrono::high_resolution_clock::now();
 
@@ -39,7 +76,8 @@ ExecuteResult execute_app(const std::string &arguments)
         programFinished = true;
     });
 
-    while (!programFinished && ((std::chrono::high_resolution_clock::now() - start) < std::chrono::milliseconds(500)))
+    while (!programFinished && ((std::chrono::high_resolution_clock::now() - start) <
+std::chrono::milliseconds(500)))
     {
     }
     if (!programFinished)
@@ -50,7 +88,7 @@ ExecuteResult execute_app(const std::string &arguments)
     }
     std::cout << appResult << '\n';
     return {appResult, result.retcode(), false, false};
-}
+}*/
 
 bool program_output_pass(const Tests::Configuration::ExpectedResults &expected, const ExecuteResult &exeResults)
 {
@@ -61,13 +99,15 @@ bool program_output_pass(const Tests::Configuration::ExpectedResults &expected, 
     // Look if the rejected keyword pops up in the input
     if (expected.rejected.size() > 0)
     {
-        if (!std::all_of(expected.rejected.begin(), expected.rejected.end(), [&](const auto &lookFor) {
+        if (std::none_of(expected.rejected.begin(), expected.rejected.end(), [&](const auto &lookFor) {
                 auto result = exeResults.data.find(lookFor, 0);
-                if (result != std::string::npos)
+                if (result == std::string::npos)
                     return false;
                 return true;
             }))
+        {
             return false;
+        }
     }
 
     // Look for the answers now
@@ -87,7 +127,7 @@ bool program_output_pass(const Tests::Configuration::ExpectedResults &expected, 
 
     return std::all_of(answers.begin(), answers.end(), [&](const auto &lookFor) {
         auto result = exeResults.data.find(lookFor, 0);
-        if (result != std::string::npos)
+        if (result == std::string::npos)
             return false;
         return true;
     });
@@ -110,9 +150,11 @@ TestResult run_test(const Tests::Configuration::ExpectedResults &expected, std::
     }
 
     const std::string cmd = std::format("{} --load {} --guess {}", app, expected.filename, guesses);
+    std::vector<std::string> cmdVector{app, "--load", expected.filename, "--guess", guesses};
+
     std::cout << "Running test: " << cmd << '\n';
     auto start = std::chrono::high_resolution_clock::now();
-    auto ret = execute_app(cmd);
+    auto ret = execute_app(cmdVector);
     auto end = std::chrono::high_resolution_clock::now();
 
     TestResult result{std::chrono::duration_cast<std::chrono::milliseconds>(end - start), expected.test, ret.data,
