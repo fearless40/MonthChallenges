162329b0ca7866d4c93b52d59711fde1b683691b
Adam Spivack <spivack@outlook.com>
2025-01-06 16:31:53 -0800

    Run tests can now run the program and recover from a frozen program. However it does not accuratly gauge the results of the program. Todo fix it

--- AppTest/source/runtests.cpp
+++ AppTest/source/runtests.cpp
@@ -3,21 +3,98 @@
 #include "TestDefinition.hpp"
 #include "subprocess.hpp"
 #include <algorithm>
+#include <atomic>
 #include <chrono>
 #include <iostream>
-#include <stdio.h>
+#include <thread>
 
 struct TestResult
 {
     std::chrono::milliseconds timeToRun;
-    Tests::Definition &def;
+    const Tests::Definition &def;
     std::string programOutput;
     bool passed;
 };
 
-void run_test(const Tests::Configuration::ExpectedResults &expected, std::string &app)
+struct ExecuteResult
+{
+    std::string data;
+    int programReturnCode{0};
+    bool programError{false};
+    bool timeOut{false};
+};
+
+ExecuteResult execute_app(const std::string &arguments)
 {
     namespace sp = subprocess;
+    auto result = sp::Popen(arguments, sp::output{sp::PIPE}, sp::bufsize{2048});
+    std::string appResult;
+    std::atomic<bool> programFinished{false};
+    /* Read pipe until end of file, or an error occurs. */
+    // If it takes longer than 1 min then quit with a time out.
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::jthread runSubprocess([&]() {
+        appResult = result.communicate().first.buf.data();
+        programFinished = true;
+    });
+
+    while (!programFinished && ((std::chrono::high_resolution_clock::now() - start) < std::chrono::milliseconds(500)))
+    {
+    }
+    if (!programFinished)
+    {
+        std::cout << "Attempting to kill the application \n";
+        result.kill();
+        return {appResult, result.retcode(), false, true};
+    }
+    std::cout << appResult << '\n';
+    return {appResult, result.retcode(), false, false};
+}
+
+bool program_output_pass(const Tests::Configuration::ExpectedResults &expected, const ExecuteResult &exeResults)
+{
+
+    if (exeResults.programError || exeResults.timeOut)
+        return false;
+
+    // Look if the rejected keyword pops up in the input
+    if (expected.rejected.size() > 0)
+    {
+        if (!std::all_of(expected.rejected.begin(), expected.rejected.end(), [&](const auto &lookFor) {
+                auto result = exeResults.data.find(lookFor, 0);
+                if (result != std::string::npos)
+                    return false;
+                return true;
+            }))
+            return false;
+    }
+
+    // Look for the answers now
+    // Generate answers to search for
+    std::vector<std::string> answers;
+    for (const Tests::QueryAnswer &q : expected.expected)
+    {
+        if (q.is_oob)
+        {
+            answers.emplace_back("OOB");
+        }
+        else
+        {
+            answers.emplace_back(std::format("{}", q.answer));
+        }
+    }
+
+    return std::all_of(answers.begin(), answers.end(), [&](const auto &lookFor) {
+        auto result = exeResults.data.find(lookFor, 0);
+        if (result != std::string::npos)
+            return false;
+        return true;
+    });
+}
+
+TestResult run_test(const Tests::Configuration::ExpectedResults &expected, std::string &app)
+{
 
     std::string guesses;
     guesses.reserve(512);
@@ -27,60 +104,22 @@ void run_test(const Tests::Configuration::ExpectedResults &expected, std::string
         guesses.append(guess.as_base26_fmt());
         guesses.append(" ");
     }
-
-    std::vector<std::string> arguments{app, "--load", expected.filename, "--guess"};
-    if (expected.queries.size() > 0)
+    if (expected.queries.size() == 0)
     {
-
-        arguments.push_back(guesses);
-    }
-    else
-    {
-        arguments.push_back("a0");
-    }
-    std::string arg2;
-    for (const auto &st : arguments)
-    {
-        arg2 += st + " ";
-    };
-
-    std::cout << "Running the following test: " << arg2 << '\n';
-    // auto result = sp::check_output(arguments);
-    // auto result = sp::Popen(arguments, sp::output{sp::PIPE});
-    // auto buf = result.communicate().first;
-    // std::cout << buf.buf.data() << '\n';
-
-    char psBuffer[128];
-    FILE *pPipe;
-
-    /* Run DIR so that it writes its output to a pipe. Open this
-     * pipe with read text attribute so that we can read it
-     * like a text file.
-     */
-
-    if ((pPipe = _popen(arg2.c_str(), "rt")) == NULL)
-    {
-        exit(1);
+        guesses.append("a0");
     }
 
-    /* Read pipe until end of file, or an error occurs. */
+    const std::string cmd = std::format("{} --load {} --guess {}", app, expected.filename, guesses);
+    std::cout << "Running test: " << cmd << '\n';
+    auto start = std::chrono::high_resolution_clock::now();
+    auto ret = execute_app(cmd);
+    auto end = std::chrono::high_resolution_clock::now();
 
-    while (fgets(psBuffer, 128, pPipe))
-    {
-        std::cout << psBuffer;
-    }
+    TestResult result{std::chrono::duration_cast<std::chrono::milliseconds>(end - start), expected.test, ret.data,
+                      program_output_pass(expected, ret)};
 
-    int endOfFileVal = feof(pPipe);
-    int closeReturnVal = _pclose(pPipe);
-
-    if (endOfFileVal)
-    {
-        printf("\nProcess returned %d\n", closeReturnVal);
-    }
-    else
-    {
-        printf("Error: Failed to read the pipe to the end.\n");
-    }
+    std::cout << std::format("Passed: [{}] Took: [{}] \n", result.passed, result.timeToRun);
+    return result;
 }
 
 std::vector<TestResult> run_all_tests(Tests::Configuration &config, std::filesystem::path app)
@@ -90,11 +129,10 @@ std::vector<TestResult> run_all_tests(Tests::Configuration &config, std::filesys
     std::string appString = app.generic_string();
     for (auto const &expected : config)
     {
-        run_test(expected, appString);
-        // results.push_back(run_test(expected, app));
+        results.push_back(run_test(expected, appString));
     }
 
-    return {};
+    return results;
 }
 
 int main_run_tests(std::filesystem::path testPath, std::filesystem::path exe)
