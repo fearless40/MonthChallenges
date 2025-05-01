0597b321fe94b925537023f280fbb38b0f8b4312
Fearless40 <spivack@gmail.com>
2025-05-01 15:35:30 -0700

    Got my external lib working through a link.

--- Challenge03/tester03/src/runner.cpp
+++ Challenge03/tester03/src/runner.cpp
@@ -1,5 +1,8 @@
 #include "runner.hpp"
+#include "programoptions.hpp"
 #include "reproc++/reproc.hpp"
+#include <charconv>
+#include <cstddef>
 #include <iostream>
 #include <system_error>
 
@@ -11,52 +14,83 @@ void stripn(unsigned char *buff, std::size_t size) {
   std::cout << ',';
 }
 
-bool test(ProgramOptions::Options const &opt) {
-  reproc::process subapp;
-  reproc::options o;
+std::optional<std::size_t>
+get_ai_number_from_app(ProgramOptions::Options const &opt) {
+  reproc::process app;
+  reproc::options options;
   reproc::stop_actions stop = {
       {reproc::stop::noop, reproc::milliseconds(0)},
       {reproc::stop::terminate, reproc::milliseconds(5000)},
       {reproc::stop::kill, reproc::milliseconds(2000)}};
 
-  o.stop = stop;
-  o.redirect.out.type = reproc::redirect::pipe;
-  o.redirect.in.type = reproc::redirect::pipe;
+  std::array<std::string, 2> cmdline{opt.program_to_test, "ai"};
+  options.stop = stop;
+  options.redirect.out.type = reproc::redirect::pipe;
+  options.redirect.in.type = reproc::redirect::pipe;
 
-  std::array<std::string, 4> cmdline{opt.program_to_test, "run", "--ai", "0"};
-  auto ec = subapp.start(cmdline, o);
+  std::error_code ec = app.start(cmdline, options);
 
-  if (ec == std::errc::no_such_file_or_directory) {
-    std::cout << "Program not found.\n";
-    return false;
+  if (ec == std::errc::no_such_file_or_directory)
+    return {};
+
+  unsigned char buff[255];
+  std::size_t bytes_read;
+  std::tie(bytes_read, ec) =
+      app.read(reproc::stream::in, (unsigned char *)&buff, 254);
+  if (!ec && bytes_read != 0) {
+    std::size_t nbrAi{0};
+    auto result = std::from_chars(buf, buf + bytes_read, nbrAi);
+      if( result.
+
+  return {};
   }
 
-  unsigned char buffer[256];
+  bool test(ProgramOptions::Options const &opt) {
+    reproc::process subapp;
+    reproc::options o;
+    reproc::stop_actions stop = {
+        {reproc::stop::noop, reproc::milliseconds(0)},
+        {reproc::stop::terminate, reproc::milliseconds(5000)},
+        {reproc::stop::kill, reproc::milliseconds(2000)}};
 
-  for (int i = 0; i < 5; ++i) {
+    o.stop = stop;
+    o.redirect.out.type = reproc::redirect::pipe;
+    o.redirect.in.type = reproc::redirect::pipe;
+
+    std::array<std::string, 4> cmdline{opt.program_to_test, "run", "--ai", "0"};
+    auto ec = subapp.start(cmdline, o);
+
+    if (ec == std::errc::no_such_file_or_directory) {
+      std::cout << "Program not found.\n";
+      return false;
+    }
+
+    unsigned char buffer[256];
+
+    for (int i = 0; i < 5; ++i) {
+      std::size_t sz;
+      std::tie(sz, ec) =
+          subapp.read(reproc::stream::out, (unsigned char *)&buffer, 255);
+      stripn(buffer, sz);
+
+      subapp.write((unsigned char *)&"M\n", 2);
+    }
+
+    subapp.write((unsigned char *)&"Q\n", 2);
+
     std::size_t sz;
     std::tie(sz, ec) =
         subapp.read(reproc::stream::out, (unsigned char *)&buffer, 255);
     stripn(buffer, sz);
 
-    subapp.write((unsigned char *)&"M\n", 2);
+    o.stop.first = {reproc::stop::wait, reproc::milliseconds(10000)};
+
+    int status = 0;
+    std::tie(status, ec) = subapp.stop(o.stop);
+
+    if (ec) {
+      std::cout << "Error: " << ec.message() << '\n';
+    }
+
+    return true;
   }
-
-  subapp.write((unsigned char *)&"Q\n", 2);
-
-  std::size_t sz;
-  std::tie(sz, ec) =
-      subapp.read(reproc::stream::out, (unsigned char *)&buffer, 255);
-  stripn(buffer, sz);
-
-  o.stop.first = {reproc::stop::wait, reproc::milliseconds(10000)};
-
-  int status = 0;
-  std::tie(status, ec) = subapp.stop(o.stop);
-
-  if (ec) {
-    std::cout << "Error: " << ec.message() << '\n';
-  }
-
-  return true;
-}
