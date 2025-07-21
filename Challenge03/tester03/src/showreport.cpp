1ac9b886fc51b985d5af730fcc148177014a923f
Fearless40 <spivack@gmail.com>
2025-07-21 16:21:13 -0700

    Messing with my tabs, still not fully working. Trying to fill out the UI too.

--- Challenge03/tester03/src/showreport.cpp
+++ Challenge03/tester03/src/showreport.cpp
@@ -4,6 +4,7 @@
 #include "programoptions.hpp"
 #include "virtualgames.hpp"
 #include <atomic>
+#include <cstddef>
 #include <format>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_base.hpp>
@@ -21,38 +22,18 @@ ftxui::Component ToggleButton(std::string const &label, int tabID,
                               bool show_close = true);
 
 namespace AppTabs {
-static int selected;
+static std::size_t selected;
+static int fake_selected;
 
 static ftxui::Component tabs_c;
 static ftxui::Component container_c;
 
-static void add_tab(std::string label, bool show_close, ftxui::Component comp) {
-  if (!tabs_c) {
-    tabs_c = ftxui::Container::Horizontal({ToggleButton(label, 0, show_close)});
-    container_c = ftxui::Container::Tab({comp}, &selected);
-  } else {
-    tabs_c->Add(ToggleButton(label, show_close, tabs_c->ChildCount() + 1));
-    container_c->Add(comp);
-  }
-}
-
-// static void remove_tab(std::string label) {
-//   auto it = std::ranges::find(labels, label);
-//   if (it != labels.end()) {
-//     auto distance = std::distance(labels.begin(), it);
-//     labels.erase(it);
-//     components.erase(components.begin() + distance);
-//     ftxui::ScreenInteractive::Active()->PostEvent(
-//         ftxui::Event::Custom); // force redraw
-//   }
-// }
-
-static void set_active_tab(int tabID) {
+static void set_active_tab(std::size_t tabID) {
   selected = tabID;
   container_c->SetActiveChild(container_c->ChildAt(selected));
 }
 
-static void close_tab(int tabID) {
+static void close_tab(std::size_t tabID) {
   if (container_c->ChildCount() == 1 || tabID >= container_c->ChildCount())
     return;
 
@@ -67,6 +48,17 @@ static void close_tab(int tabID) {
   tabs_c->SetActiveChild(tabs_c->ChildAt(tabID - 1));
 
   // remove tab
+}
+static void add_tab(std::string label, bool show_close, ftxui::Component comp) {
+  if (!tabs_c) {
+
+    tabs_c = ftxui::Container::Horizontal({ToggleButton(label, 0, show_close)});
+    container_c = ftxui::Container::Tab({comp}, &fake_selected);
+  } else {
+    tabs_c->Add(ToggleButton(label, show_close, tabs_c->ChildCount() + 1));
+    container_c->Add(comp);
+    AppTabs::set_active_tab(tabs_c->ChildCount() - 1);
+  }
 }
 
 static void init() { selected = 0; }
@@ -118,17 +110,16 @@ void AiButtonClick(AIID id) {}
 //
 ftxui::Component overview_tab(std::vector<VirtualGames> const &games) {
   using namespace ftxui;
-  // auto header = hbox({text(std::format("Total time: {}", 5)),
-  // separator(),
-  //                     text(std::format("Total AIs Run: {}",
-  //                     games.size()))});
-  //
-  // std::vector<std::string> ColHeaders = {"AI", "Average Guesses",
-  // "Won", "Lost",
-  //                                        "Repeats"};
-  // std::vector<std::vector<std::string>> rows;
-  // std::vector<Element> ai_details;
+  auto header = hbox({text(std::format("Total time: {}", 5)), separator(),
+                      text(std::format("Total AIs Run: {}", games.size())),
+                      separator(), text(std::format("Iterations: {}", 10))});
+
+  std::vector<std::string> ColHeaders = {"AI", "Average Guesses", "Won", "Lost",
+                                         "Repeats"};
+  std::vector<std::vector<std::string>> rows;
   Components ai_buttons;
+
+  rows.push_back(std::move(ColHeaders));
 
   for (auto const &game : games) {
     std::string buttonlbl = std::format("AI {}", game.aiid());
@@ -144,29 +135,31 @@ ftxui::Component overview_tab(std::vector<VirtualGames> const &games) {
         });
     ai_buttons.push_back(button_row);
 
-    // rows.push_back({std::to_string(game.aiid()),
-
-    //                       VirtualGames::EndingState::sunk_all_ships)),
-    //                   std::to_string(game.global_stats().ending_state(
-    //                       VirtualGames::EndingState::too_many_guess)),
-    //                   std::to_string(game.global_stats().repeat_guess_count)});
+    rows.push_back({std::to_string(game.aiid()),
+                    std::to_string(game.global_stats().average_guess_count),
+                    std::to_string(game.global_stats().ending_state(
+                        VirtualGames::EndingState::sunk_all_ships)),
+                    std::to_string(game.global_stats().ending_state(
+                        VirtualGames::EndingState::too_many_guess)),
+                    std::to_string(game.global_stats().repeat_guess_count)});
   };
 
   auto vert = Container::Vertical(ai_buttons);
-  // auto table = ftxui::Table(rows);
-  // table.SelectAll().Border(BorderStyle::LIGHT);
-  // auto fullview = Renderer(vert, [&]() {
-  //   Elements internal{header, separator(), text("AI Overview")};
-  //   for (auto &button : ai_buttons) {
-  //     internal.push_back(button->Render());
-  //   };
-  //   internal.push_back(separator());
-  //   // internal.push_back(table.Render());
-  //
-  //   return vbox(internal);
-  // });
-  //
-  return vert;
+  auto table = ftxui::Table(rows);
+  table.SelectAll().Border(BorderStyle::LIGHT);
+  table.SelectAll().Decorate(vcenter);
+  table.SelectAll().Decorate(hcenter);
+  table.SelectColumn(0).Decorate(bold);
+  table.SelectRow(0).Decorate(bold);
+  table.SelectAll().Border(LIGHT);
+  table.SelectAll().Separator(LIGHT);
+
+  auto tableElement = table.Render();
+
+  return Renderer(vert, [header, vert, tableElement]() -> Element {
+    return vbox({header, separator(), text("AI Overview") | bold,
+                 vert->Render(), separator(), tableElement});
+  });
 }
 
 void start(ProgramOptions::Options const &opt, std::vector<VirtualGames> &games)
@@ -188,7 +181,9 @@ void start(ProgramOptions::Options const &opt, std::vector<VirtualGames> &games)
          separator()});
   });
 
-  auto ButtonTest = Button("Yo", {});
+  auto ButtonTest = Button("Yo", [] {
+    AppTabs::add_tab("Garabage", true, ftxui::Button("Hello me", {}));
+  });
 
   AppTabs::add_tab("Overview", false, overview_tab(games));
   AppTabs::add_tab("Test", true, ButtonTest);
